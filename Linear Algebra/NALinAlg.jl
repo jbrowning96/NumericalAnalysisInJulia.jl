8cea89967f5d44bf0f9ee927cc82186281b29af7
Justin <jtbrowning1996@icloud.com>
2023-02-22 19:57:17 -0600

    Adding Gauss Jordan with pivot and beginning to add iterative methods

--- Linear Algebra/NALinAlg.jl
+++ Linear Algebra/NALinAlg.jl
@@ -9,10 +9,9 @@ function NaiveGaussJordan(A::AbstractArray)
         println("Matrix is not square, cannot perform Gauss Jordan")
     end
 
+    # Reducing the matrix by taking making all values under the row a 0, so as to upper triangularize the system.
     pivot = 0
     target = 0
-    # Reducing the matrix by taking making all values under the row a 0, so as to upper triangularize the system.
-
     for j in range(n-1)
         if abs(A[j,j])isapprox(0)
             error("Zero Pivot Encountered")
@@ -23,23 +22,33 @@ function NaiveGaussJordan(A::AbstractArray)
             pivot = A[j,j]
             target = A[j+1,j]
             mult = target/pivot
-            A[j+1,:] = (mult)*A[i,:]
+            A[j+1,:] = copy((mult)*A[i,:])
         end
     end
 end
 
-function GaussJordan(A::AbstractArray)
+function GaussJordanPartialPivot(A::AbstractArray)
     #Checking for square matrix
     if size(A)(1) == size(A)(2)
         n = size(A)(1)
     else
         println("Matrix is not square, cannot perform Gauss Jordan")
     end
+    #Doing any exchanges so that the largest value for this diagonal exists in the a(j,j)th diagonal.
+    for i in range(n)
+        for j in range(n)
+            if A[i,j] > A[i,i]
+                #Swap
+                temp = copy(A[i,:])
+                A[i,:] = copy(A[j,:])
+                A[j,:] = copy(temp)
+            end
+        end
+    end
 
+    # Reducing the matrix by taking making all values under the row a 0, so as to upper triangularize the system.
     pivot = 0
     target = 0
-    # Reducing the matrix by taking making all values under the row a 0, so as to upper triangularize the system.
-    # This means we take
     for j in range(n-1)
         if abs(A[j,j])isapprox(0)
             error("Zero Pivot Encountered")
@@ -50,7 +59,14 @@ function GaussJordan(A::AbstractArray)
             pivot = A[j,j]
             target = A[j+1,j]
             mult = target/pivot
-            A[j+1,:] = (mult)*A[i,:]
+            A[j+1,:] = copy((mult)*A[i,:])
         end
     end
+end
+
+function JacobiMethod(A::AbstractArray)
+end
+function GaussSedal(A::AbstractArray)
+end
+function SuccessiveOverRelaxation(A::AbstractArray, ω)
 end
